8c9f9c0b0153c4feb249df4d03bff96c14916eeb
william caouette <william.caouette2001@gmail.com>
2021-01-13 09:35:57 -0500

    gestionBoostPoits

--- GamejamRebellion/Assets/Scripts/gestionBoostPoints.cs
+++ GamejamRebellion/Assets/Scripts/gestionBoostPoints.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class gestionBoostPoints : MonoBehaviour
 {
     // CE SCRIPT EST TEMPORAIRE IL SERA IMPLÉMENTER DANS LE GAME MANAGER
-
+    
 
     void Start()
     {
@@ -16,5 +16,30 @@ public class gestionBoostPoints : MonoBehaviour
     void Update()
     {
         
+    }
+
+    //fonction appeler quand le joueur touche la cible
+    public int CalculerPoints(int nbPoints)
+    {
+        int multiplicateur;
+        switch (GetComponent<gestionTimeScale>().boostText)
+        {
+            case "X1":
+                multiplicateur = 1;
+                break;
+            case "X2":
+                multiplicateur = 2;
+                break;
+            case "X3":
+                multiplicateur = 3;
+                break;
+            case "X4":
+                multiplicateur = 4;
+                break;
+            default:
+                multiplicateur = 1;
+                break;
+        }
+        return nbPoints * multiplicateur;
     }
 }
