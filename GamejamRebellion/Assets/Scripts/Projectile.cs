c17eed80c83dcf423c7e134f5e6217192b36e4fa
william caouette <william.caouette2001@gmail.com>
2021-01-13 14:45:05 -0500

    projectil couleur

--- GamejamRebellion/Assets/Scripts/Projectile.cs
+++ GamejamRebellion/Assets/Scripts/Projectile.cs
@@ -18,9 +18,28 @@ public class Projectile : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    { }
+    {
+        switch (Time.timeScale)
+        {
+            case 1:
+                this.GetComponentInChildren<TrailRenderer>().endColor = Color.white;
+                break;
+            case 2:
+                this.GetComponentInChildren<TrailRenderer>().endColor = Color.yellow;
+                break;
+            case 3:
+                this.GetComponentInChildren<TrailRenderer>().endColor = Color.red;
+                break;
+            case 4:
+                this.GetComponentInChildren<TrailRenderer>().endColor = Color.blue;
+                break;
+            default:
+                this.GetComponentInChildren<TrailRenderer>().endColor = Color.white;
+                break;
+        }
+    }
 
-    private void OnCollisionEnter(Collision collision) {
+        private void OnCollisionEnter(Collision collision) {
         if (statue != null && gameController != null && statue.GetInstanceID() == collision.gameObject.GetInstanceID()) {
             Debug.Log("Le projectile "+gameObject.name+" est entrer en collision avec la statue!");
             gameController.touchee(poids);
