7726ad903d3122aae8b929702b49471c6f78ae0f
william caouette <william.caouette2001@gmail.com>
2021-01-13 09:16:03 -0500

    creation du script temporaire
    script temporaire qui sera implémenter dans les game controller

---
+++ GamejamRebellion/Assets/Scripts/gestionBoostPoints.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gestionBoostPoints : MonoBehaviour
+{
+    // CE SCRIPT EST TEMPORAIRE IL SERA IMPLÉMENTER DANS LE GAME MANAGER
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
