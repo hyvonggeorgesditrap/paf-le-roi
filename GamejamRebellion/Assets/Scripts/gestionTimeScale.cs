4f5a0c06499268d493c3d7758806ac54ddd7703d
william caouette <william.caouette2001@gmail.com>
2021-01-13 00:24:30 -0500

    pause play
    pause play

--- GamejamRebellion/Assets/Scripts/gestionTimeScale.cs
+++ GamejamRebellion/Assets/Scripts/gestionTimeScale.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class gestionTimeScale : MonoBehaviour
 {
    private int i;
+   public string boostText = "X1";
 
    public void AugmenterTemps()
    {
@@ -13,10 +14,29 @@ public class gestionTimeScale : MonoBehaviour
         {
             Time.timeScale += 0.2f;
             i = 0;
+            if (Time.timeScale == 2)
+            {
+                boostText = "X2";
+            }else if (Time.timeScale == 3)
+            {
+                boostText = "X3";
+            }
+            else if (Time.timeScale == 4)
+            {
+                boostText = "X4";
+            }
         }
    }
 
     public void ReduireVitesse()
+    {
+        Time.timeScale = 1;
+    }
+    public void Pause()
+    {
+        Time.timeScale = 0;
+    }
+    public void Play()
     {
         Time.timeScale = 1;
     }
