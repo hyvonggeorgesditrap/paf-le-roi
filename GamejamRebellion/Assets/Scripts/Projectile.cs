61dc6592953babe0eb5b6fe6e1ac749a1f56a042
william caouette <william.caouette2001@gmail.com>
2021-01-13 15:21:33 -0500

    couleur pas fini

--- GamejamRebellion/Assets/Scripts/Projectile.cs
+++ GamejamRebellion/Assets/Scripts/Projectile.cs
@@ -6,7 +6,7 @@ public class Projectile : MonoBehaviour
 {
     [SerializeField]
     public int poids;
-
+    public Color couleurTrainer;
     private GameObject statue = null;
     private GameController gameController = null;
 
@@ -14,29 +14,27 @@ public class Projectile : MonoBehaviour
     void Start() {
         statue = FindObjectOfType<Statue>().gameObject;
         gameController = FindObjectOfType<GameController>();
+        couleurTrainer = gameObject.GetComponentInChildren<TrailRenderer>().endColor;
     }
 
     // Update is called once per frame
     void Update()
     {
-        switch (Time.timeScale)
+        Debug.Log(couleurTrainer);
+        if(Time.timeScale < 1.2)
         {
-            case 1:
-                this.GetComponentInChildren<TrailRenderer>().endColor = Color.white;
-                break;
-            case 2:
-                this.GetComponentInChildren<TrailRenderer>().endColor = Color.yellow;
-                break;
-            case 3:
-                this.GetComponentInChildren<TrailRenderer>().endColor = Color.red;
-                break;
-            case 4:
-                this.GetComponentInChildren<TrailRenderer>().endColor = Color.blue;
-                break;
-            default:
-                this.GetComponentInChildren<TrailRenderer>().endColor = Color.white;
-                break;
+            couleurTrainer = Color.white;
+        }else if (Time.timeScale < 3)
+        {
+            couleurTrainer = Color.yellow;
+        }else if (Time.timeScale < 4)
+        {
+            couleurTrainer = Color.red;
+        }else
+        {
+            couleurTrainer = Color.blue;
         }
+        
     }
 
         private void OnCollisionEnter(Collision collision) {
