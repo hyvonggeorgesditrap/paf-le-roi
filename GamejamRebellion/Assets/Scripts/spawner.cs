f5d3f1f603acf43d9b7e9da597bd995c07381016
william caouette <william.caouette2001@gmail.com>
2021-01-13 16:04:29 -0500

    min vecteur initial spawner

--- GamejamRebellion/Assets/Scripts/spawner.cs
+++ GamejamRebellion/Assets/Scripts/spawner.cs
@@ -10,6 +10,11 @@ public class spawner : MonoBehaviour
     private List<GameObject> ListObjets;
     [SerializeField]
     private Camera cam;
+    [SerializeField]
+    private int maxVecteur = 3;
+    [SerializeField]
+    private int minVecteur = 8;
+
 
     // Start is called before the first frame update
     void Start()
@@ -28,7 +33,7 @@ public class spawner : MonoBehaviour
         int spawnerID = Random.Range(0, spawners.Count);
         int objectID = Random.Range(0, ListObjets.Count);
         GameObject objet = Instantiate(ListObjets[objectID], spawners[spawnerID].transform.position, Quaternion.identity);
-        int velocite = Random.Range(5, 8);
+        int velocite = Random.Range(minVecteur, maxVecteur);
         Vector3 target = new Vector3(cam.transform.position.x, cam.transform.position.y + 2, spawners[spawnerID].transform.position.z);
         Vector3 impulse = velocite * (target - objet.transform.position);
         objet.GetComponent<Rigidbody>().AddForce(impulse, ForceMode.Impulse);
