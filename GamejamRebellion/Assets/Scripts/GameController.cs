bd0ca58aaf115770b64f1bf1d7ae9149eede4cb9
GVHLNC <hlnc.app@gmail.com>
2021-01-13 02:51:18 -0500

    TMPro + text
    Importation de TMPro

--- GamejamRebellion/Assets/Scripts/GameController.cs
+++ GamejamRebellion/Assets/Scripts/GameController.cs
@@ -1,21 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameController : MonoBehaviour
 {
-    public float score;
+    public int score;
+    public int bestScore;
+
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI bestScoreText;
+    public TextMeshProUGUI comboText;
 
     private bool gamePlaying;
     public void BeginGame()
     {
         Debug.Log("Le jeu se lance");
+        
+
         //gamePlaying = true;
     }
 
     void Update()
     {
-        
+        scoreText.text = score.ToString();
+        bestScoreText.text = "Meilleur score : " + bestScore.ToString();
+        comboText.text = "Combo";
     }
 
     public void AddScore(int a_objet)
