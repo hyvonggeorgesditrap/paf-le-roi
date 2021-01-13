3c40ca3d459ca1e867fe69fa7c2338ae2fe65c61
Mdaiglevigneault <44245132+Mdaiglevigneault@users.noreply.github.com>
2021-01-13 11:42:59 -0500

    Refonte Health

--- GamejamRebellion/Assets/Scripts/GameController.cs
+++ GamejamRebellion/Assets/Scripts/GameController.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class GameController : MonoBehaviour
 {
+    public float health;
+    public float maxHealth;
+
     public int score;
     public int bestScore;
 
@@ -13,12 +17,13 @@ public class GameController : MonoBehaviour
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI bestScoreText;
     public TextMeshProUGUI comboText;
+    public Image healthBarImage;
 
     private bool gamePlaying;
     public void BeginGame()
     {
         Debug.Log("Le jeu se lance");
-
+        updateAffichageHealth();
         //gamePlaying = true;
 
     }
@@ -51,10 +56,25 @@ public class GameController : MonoBehaviour
         score += a_objet;
     }
 
+    public void addHealth(int damageAmout)
+    {
+        health += damageAmout;
+        health = Mathf.Clamp(health, 0f, maxHealth);
+        updateAffichageHealth();
+    }
+
+    void updateAffichageHealth()
+    {
+        healthBarImage.fillAmount = health / maxHealth;
+    }
+
     void OnTriggerExit(Collider other) {
         // Destroy Projectile that leaves the trigger
         if (other.gameObject.tag == "Projectile") {
-            Debug.Log("Projectile detruit!");
+
+            //Reduire la vie
+            addHealth(0 - other.GetComponent<Projectile>().poids);
+            //Detruire l'objet
             Destroy(other.gameObject);
         }
     }
